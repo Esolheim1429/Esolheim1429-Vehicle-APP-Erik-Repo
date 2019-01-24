928ab4edf2a7908c8957288a414992177c390d1e
period3 <period3@p-162mba-21.local>
2019-01-24 10:14:54 -0600

    test

--- Vehicle App/AppDelegate.swift
+++ Vehicle App/AppDelegate.swift
@@ -17,7 +17,7 @@ import GoogleSignIn
     
     var signedIn = false
     var window: UIWindow?
-
+//
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
